81bde4e0a04035da3abec5d65be6552f22ed0ffd
iamsven2005 <iamsven2005@gmail.com>
2024-05-03 01:00:07 +0800

    Debug Drizzle

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,7 +1,13 @@
-const Cources = () => {
+import { getCourses } from "@/db/queries";
+
+const Cources = async () => {
+    const data = await getCourses();
     return ( 
-        <section>
-                dtf
+        <section className="h-full max-w-[912px] px-3 mx-auto">
+            <div className="text-2xl font-bol">
+                All courses
+            </div>
+            {JSON.stringify(data)}
         </section>
      );
 }
--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,5 +1,6 @@
 import { neon } from "@neondatabase/serverless"
 import * as schema from "./schema"
+import { drizzle } from "drizzle-orm/neon-http";
 const sql = neon(process.env.DATABASE_URL!);
 //@ts-ignore
 const db = drizzle(sql, { schema })
